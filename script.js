99de8838118dc58082746e27f30ed85600b41530
Rene Promesse <nrenejordan1@gmail.com>
2022-02-07 21:09:26 +0200

    created bookmark

---
+++ script.js
@@ -0,0 +1,9 @@
+(
+    function(){
+        console.log("Hello World");
+        alert("Hello World");
+    }
+)()
+
+
+// (function(){let script = document.createElement("script");script.src = "script.js";document.body.appendChild(script);})
