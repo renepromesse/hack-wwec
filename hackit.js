917cdf4130e2951d965d57ce5ef200a8f6fb56bf
Rene Promesse <nrenejordan1@gmail.com>
2022-02-07 22:35:30 +0200

    adds the script for wwec

---
+++ hackit.js
@@ -0,0 +1,17 @@
+(
+    function(){
+        let p = document.getElementsByClassName("value")[0];
+        let callTrack = setInterval(getKey, 100);
+        function getKey(){
+            p = document.getElementsByClassName("value")[0];
+            console.log("key ---> " + p.innerHTML);
+            if(p.innerHTML >0){
+                confirm('Confrm someone is coming ...') ? clearLoop()   : window.location.reload();
+            }
+            function clearLoop(){
+                clearInterval(callTrack);
+            }
+        }
+            
+    }
+)()
